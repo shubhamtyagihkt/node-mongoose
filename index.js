9e64d0e37c99b9c770386cbfdeebfa6eb2131150
Shubham Tyagi <shubhamtyagihkt@gmail.com>
2020-09-06 09:44:09 +0530

    mongoose subdocument, add comment option

--- index.js
+++ index.js
@@ -3,18 +3,35 @@ const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
 
-const connect = mongoose.connect(url, { useUnifiedTopology: true });
+const connect = mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
 
 connect.then((db) => {
     console.log('Connected correctly to server');
 
     Dishes.create({
-        name: "Aalo Paratha",
+        name: "Aalo Paneer Paratha",
         description: "Famous food in Delhi"
     })
     .then((dish) => {
         console.log(dish);
-        return Dishes.find({});
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
     })
     .then((dishes) => {
         console.log(dishes);
